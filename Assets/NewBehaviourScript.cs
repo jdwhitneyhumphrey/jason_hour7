9761788ee4c35b3194afed878d8647c29a4bbdde
BT100 Building <BT100@ac.sdmesa.net>
2023-03-21 10:35:21 -0700

    changes

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,27 +7,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Put together a loop that outputs 9 - 0
-        int count = 9;
-        //Debug.Log("This is my count" + count)
-        do
-        {
-            count = count - 1;
-            Debug.Log("This is my count " + count);
-        }
-        while (count > 0);
+        
 
-        count = 9;
-        //Debug.Log("This is my count" + count)
-        while (count > 0)
+        for(int i = 22; i < 100; i = i + 2)
         {
-            count = count - 1;
-            Debug.Log("This is my count " + count);
-        }
-
-        for(int i = 10; i > 0; i--)
-        {
-            Debug.Log("This is my count " + count);
+            Debug.Log(i);
         }
     }
 
